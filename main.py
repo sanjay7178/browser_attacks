2436e85c94d6dcdff5277392c9200ac9be122ec2
sanjay7178 <saisanjay7660@gmail.com>
2025-10-06 11:44:58 +0530

    added new changes to url
    Signed-off-by: sanjay7178 <saisanjay7660@gmail.com>

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from phoca2.phoca import phoca_url_detect 
 from ClickGrab.clickgrab import clickgrab_url_detect 
 from clicjack.clickjack import clickjack_url_detect
-
+from termcolor import colored
 # use async concurrent function calls 
-# print(phoca_url_detect("http://google.com")) 
-
-# print(clickgrab_url_detect("https://www.bratusferramentas.grupomoltz.com.br/"))
-
-print(clickjack_url_detect("https://1movierulzhd.top/movierulz-2025-download-telugu-hindi-tamil-kannada-malayalam-movies/"))
+print(colored("Checking MITM Vulnerability using Phoca... \n", "cyan"))
+print(phoca_url_detect("https://login.ghcat.tech")) 
+print(colored("\nChecking ClickGrab Vulnerability... \n", "cyan"))
+print(clickgrab_url_detect("https://www.bratusferramentas.grupomoltz.com.br/"))
+print(colored("\nChecking ClickJacking Vulnerability... \n", "cyan"))
+print(clickjack_url_detect("https://sanjay7178.github.io/Kubernetes-Conf/test/attacker-site.html"))
